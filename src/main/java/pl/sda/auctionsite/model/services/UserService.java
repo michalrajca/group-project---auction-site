8958a2752d4b1e86d7e76dde6e1d23569b37b0f5
mlamorska <martylamorska@gmail.com>
2019-06-01 14:53:29 +0200

    fix

--- src/main/java/pl/sda/auctionsite/model/services/UserService.java
+++ src/main/java/pl/sda/auctionsite/model/services/UserService.java
@@ -1,4 +1,40 @@
 package pl.sda.auctionsite.model.services;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.sda.auctionsite.model.entity.User;
+import pl.sda.auctionsite.model.repositories.UserRepository;
+
+@Component
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+    public void modifyUser(User user, String login) {
+        userRepository.findById(login).map(
+                userFromDatabase -> {
+                    userFromDatabase.setPassword(user.getPassword());
+                    userFromDatabase.setAccountName(user.getAccountName());
+                    userFromDatabase.setProvince(user.getProvince());
+                    userFromDatabase.setCity(user.getCity());
+                    userFromDatabase.setStreetName(user.getStreetName());
+                    userFromDatabase.setHouseNo(user.getHouseNo());
+                    userFromDatabase.setPostcode(user.getPostcode());
+                    userFromDatabase.setAvatarHref(user.getAvatarHref());
+                    return userRepository.save(userFromDatabase);
+                }
+        );
+    }
+
+    public void deleteUser(String login) {
+        userRepository.deleteById(login);
+    }
 }
