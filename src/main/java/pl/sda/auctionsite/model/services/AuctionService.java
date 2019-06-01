a10c2f69d0aec05860052115551e76789fbf7142
truthseeker <Lamelame4%>
2019-06-01 14:52:22 +0200

    modified: Auction, AuctionService created: AuctionRepository

--- src/main/java/pl/sda/auctionsite/model/services/AuctionService.java
+++ src/main/java/pl/sda/auctionsite/model/services/AuctionService.java
@@ -1,4 +1,16 @@
 package pl.sda.auctionsite.model.services;
 
+import org.springframework.stereotype.Component;
+import pl.sda.auctionsite.model.repositories.AuctionRepository;
+
+@Component
 public class AuctionService {
+
+    private final AuctionRepository auctionRepository;
+
+    public AuctionService(AuctionRepository auctionRepository) {
+        this.auctionRepository = auctionRepository;
+    }
+
+
 }
--- src/main/java/pl/sda/auctionsite/web/config/WebConfig.java
+++ src/main/java/pl/sda/auctionsite/web/config/WebConfig.java
@@ -1,4 +1,20 @@
 package pl.sda.auctionsite.web.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    ViewResolver jspViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/jsp/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
 }
