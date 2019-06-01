cd87e4226c1b00e824b2ae1b2aa2158d766da5fd
mlamorska <martylamorska@gmail.com>
2019-06-01 10:29:40 +0200

    boot

---
+++ src/main/java/pl/sda/auctionsite/AuctionSiteApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.auctionsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/sda/auctionsite/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.sda.auctionsite;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuctionSiteApplication.class);
+	}
+
+}
