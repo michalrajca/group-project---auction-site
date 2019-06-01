a64accbd819421e3cfe399ce89c359f4c9bb1cf2
truthseeker <Lamelame4%>
2019-06-01 14:58:45 +0200

    UserService fix

--- src/main/java/pl/sda/auctionsite/web/controllers/UserController.java
+++ src/main/java/pl/sda/auctionsite/web/controllers/UserController.java
@@ -1,8 +1,14 @@
 package pl.sda.auctionsite.web.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.auctionsite.model.services.UserService;
 
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
