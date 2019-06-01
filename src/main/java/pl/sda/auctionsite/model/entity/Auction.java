27dd332f5f0cc65249e0e8941ee119f972393b40
truthseeker <Lamelame4%>
2019-06-01 13:47:52 +0200

    modified: Auction,Category

--- src/main/java/pl/sda/auctionsite/model/entity/Auction.java
+++ src/main/java/pl/sda/auctionsite/model/entity/Auction.java
@@ -1,4 +1,39 @@
 package pl.sda.auctionsite.model.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
 public class Auction {
+
+    public Auction() {
+    }
+
+    String title;
+    String description;
+//    String href;
+    Category category;
+    double minimalPrice;
+    double buyPrice;
+    boolean positioned;
+    User user;
+    LocalDateTime startDate;
+    LocalDateTime endDate;
+    int count;
+
+    public Auction(String title, String description, Category category, double minimalPrice, double buyPrice, boolean positioned, User user, LocalDateTime startDate, LocalDateTime endDate, int count) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.minimalPrice = minimalPrice;
+        this.buyPrice = buyPrice;
+        this.positioned = positioned;
+        this.user = user;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.count = count;
+    }
 }
