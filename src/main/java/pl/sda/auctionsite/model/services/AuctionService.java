39862e1f72477d4164abca0c42c702805a83cd63
truthseeker <Lamelame4%>
2019-06-02 10:07:11 +0200

    Add folders

--- src/main/java/pl/sda/auctionsite/model/services/AuctionService.java
+++ src/main/java/pl/sda/auctionsite/model/services/AuctionService.java
@@ -1,7 +1,10 @@
 package pl.sda.auctionsite.model.services;
 
 import org.springframework.stereotype.Component;
+import pl.sda.auctionsite.model.entity.Auction;
 import pl.sda.auctionsite.model.repositories.AuctionRepository;
+
+import java.util.List;
 
 @Component
 public class AuctionService {
@@ -12,5 +15,27 @@ public class AuctionService {
         this.auctionRepository = auctionRepository;
     }
 
+//    public List<Auction> lastAddedAuctions(){
+//        return auctionRepository.
+//    }
 
+//    public List<Auction> endingAuctions(){
+//
+//    }
+//
+//    public List<Auction> userAuctions(){
+//
+//    }
+//
+//    public List<Auction> biddingAuctions(){
+//
+//    }
+//
+//    public List<Auction> watchListAuctions(){
+//
+//    }
+//
+//    public List<Auction> endedAuctions(){
+//
+//    }
 }
