6f9c39c9ca96c7a91dbb792d9582467b3e725534
mlamorska <martylamorska@gmail.com>
2019-06-02 14:35:15 +0200

    fix

--- src/main/java/pl/sda/auctionsite/web/controllers/UserController.java
+++ src/main/java/pl/sda/auctionsite/web/controllers/UserController.java
@@ -15,19 +15,24 @@ public class UserController {
         this.userService = userService;
     }
 
+    @RequestMapping(method = RequestMethod.GET)
+    public String user(){
+        return "userPage";
+    }
+
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public String addUser(@RequestBody User user) {
         userService.addUser(user);
-        return "user";
+        return "userPage";
     }
 
-    @RequestMapping(value = "/users/modify/{login}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/user/modify/{login}", method = RequestMethod.PUT)
     public String modifyUser(@RequestBody User user, @PathVariable String login) {
         userService.modifyUser(user, login);
-        return "user";
+        return "userPage";
     }
 
-    @RequestMapping(value = "/users/delete/{login}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/user/delete/{login}", method = RequestMethod.DELETE)
     public String deleteUser(@PathVariable String login) {
         userService.deleteUser(login);
         return "homePage";
